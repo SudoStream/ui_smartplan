597a0e84e1604d1c701a41974dacd04fdf9ccde3
Andy Boyle <andy@sudostream.io>
2017-06-23 20:58:59 +0100

    Using Props

--- client/src/main/scala/example/ScalaJSExample.scala
+++ client/src/main/scala/example/ScalaJSExample.scala
@@ -6,42 +6,59 @@ import org.scalajs.dom
 import shared.SharedMessages
 
 import scala.scalajs.js
-import scala.scalajs.js.|
 
+//noinspection TypeAnnotation
 object ScalaJSExample extends js.JSApp {
 
-  case class Props(/* TODO */)
+  //  val myComponent =
+  //    ScalaComponent.builder[Props]("MyComponent")
+  //  |
+  //
+  //  val mainHeader =
+  //    ScalaComponent.builder[String]("MainHeader")
+  //      .render_P(name => <.h1("I Am The Header"))
+  //      .build
+  //
 
-//  val myComponent =
-//    ScalaComponent.builder[Props]("MyComponent")
-//  |
-//
-//  val mainHeader =
-//    ScalaComponent.builder[String]("MainHeader")
-//      .render_P(name => <.h1("I Am The Header"))
-//      .build
-//
+  val Hello =
+    ScalaComponent.builder[String]("Hello")
+      .render_P(name => <.div("Hello there ", name))
+      .build
+
+
+  ////////////////////////////////////////////////////
+  case class Propsy(firstname: String, secondname: String)
+
+  val HelloMessage = ScalaComponent.builder[Propsy]("HiMessage")
+    .render_P((theProps) =>
+        <.strong(s"Hello there ${theProps.firstname} ${theProps.secondname}"))
+    .build
+
+  val andy = HelloMessage(Propsy("Andhee", "Boil"))
+
   val mainDiv = ScalaComponent.builder[String]("MainDiv")
     .render_P(name =>
       <.div("I Am The Main Div",
-        <.h2("and h2 here!  " + SharedMessages.timeNow)))
+        <.br,
+        <.blockquote(andy,
+          <.blockquote(". What age are you?",
+        <.h2("and h2 here!  " + SharedMessages.timeNow
+        )))))
     .build
 
   val timeFn = () => mainDiv("toodles").renderIntoDOM(dom.document.getElementById("root"))
+  ////////////////////////////////////////////////////
 
-//
-//  val Hello =
-//    ScalaComponent.builder[String]("Hello")
-//      .render_P(name => <.div("Hello there ", name))
-//      .build
 
   def main(): Unit = {
     dom.document.getElementById("scalajsShoutOut").textContent = SharedMessages.itWorks
 
-    scala.scalajs.js.Dynamic.global.setInterval(timeFn, 10)
-//    mainDiv("toodles").renderIntoDOM(dom.document.getElementById("root"))
-  }
+    scala.scalajs.js.Dynamic.global.setInterval(timeFn, 1000)
 
+
+    //    andy.renderIntoDOM(dom.document.getElementById("root"))
+    //    mainDiv("toodles").renderIntoDOM(dom.document.getElementById("root"))
+  }
 
 
 }
